3bf5f05e7406dadffb93a7be03ab754504744932
Mickaël Mounier <mandibull@gmail.com>
2016-06-15 02:54:44 -0400

    Fixing z-index for dat.GUI.

--- model.js
+++ model.js
@@ -1,11 +1,14 @@
 'use strict';
 
+// https://github.com/mrdoob/three.js/wiki/JSON-Model-format-3
+
 class Model {
     constructor() {
         this.data = {};
     }
 
     loadFromURL(url) {
+        // try https://gist.githubusercontent.com/fraguada/8ea243744961d72d61de/raw/a6e62ae64fa0645832943a7f6c00f73d29d715d8/test.js
         this.data = getJSON(url);
     }
 }
