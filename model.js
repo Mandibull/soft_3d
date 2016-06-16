bd9444dbe2138bdad1da08c2ecd34c57b26a4ce4
Mickaël Mounier <mandibull@gmail.com>
2016-06-16 00:07:22 -0400

    Now drawing filled triangles.

--- model.js
+++ model.js
@@ -1,6 +1,12 @@
 'use strict';
 
-// https://github.com/mrdoob/three.js/wiki/JSON-Model-format-3
+/*
+ https://github.com/mrdoob/three.js/wiki/JSON-Model-format-3
+
+ https://gist.githubusercontent.com/fraguada/8ea243744961d72d61de/raw/a6e62ae64fa0645832943a7f6c00f73d29d715d8/test.js
+
+ bunny.json
+*/
 
 class Model {
     constructor() {
@@ -8,7 +14,9 @@ class Model {
     }
 
     loadFromURL(url) {
-        // try https://gist.githubusercontent.com/fraguada/8ea243744961d72d61de/raw/a6e62ae64fa0645832943a7f6c00f73d29d715d8/test.js
         this.data = getJSON(url);
+        return this;
     }
 }
+
+// var model = new Model().loadFromURL('https://gist.githubusercontent.com/fraguada/8ea243744961d72d61de/raw/a6e62ae64fa0645832943a7f6c00f73d29d715d8/test.js');
--- soft3d.js
+++ soft3d.js
@@ -4,7 +4,7 @@ class FPSCounter {
     constructor(fpsNodeID) {
         this.fpsNode = document.getElementById(fpsNodeID);
         this.lastTime = 0;
-        this.samples = 30;
+        this.samples = 20;
         this.counter = 0;
     }
 
@@ -41,17 +41,22 @@ class Soft3D {
         this.polygonEdges = 8;
         this.nbPoints = null;
         this.points = null;
+        this.drawMesh = false;
+        this.drawMethod = null;
 
         this.gui = new dat.GUI();
-        this.gui.add(this, 'polygonEdges', 2, 40).step(1);
+        this.gui.add(this, 'polygonEdges', 3, 40).step(1);
+        this.gui.add(this, 'drawMesh');
     }
 
     update(elapsedTime) {
+        this.drawMethod = this.drawMesh ? this.drawPoly : this.drawFilledPoly;
         // FIXME: find how to disallow floats in dat.GUI
         var edges = Math.trunc(this.polygonEdges);
         if (edges != this.nbPoints) {
             this.nbPoints = edges;
             this.points = new Array(this.nbPoints * 4);
+            // FIXME: there is something wrong for certain values of nbPoints (19, 33, etc.)
             var step = Math.PI * 2 / this.nbPoints;
             for (var i = 0; i < this.nbPoints; ++i) {
                 this.points[i*2+0] = Math.round(
@@ -75,7 +80,7 @@ class Soft3D {
         this.update(elapsedTime);
         this.render();
 
-        // blit
+        // Blit
         this.imageData.data.set(this.buf8);
         this.ctx.putImageData(this.imageData, 0, 0);
 
@@ -85,33 +90,89 @@ class Soft3D {
     }
 
     render() {
-        // FIXME: fill is too slow
+        // FIXME: can this get faster?
         this.data.fill(0);
 
         var hue = 0;
         var hueStep = Math.floor(360 / this.nbPoints);
         for (var i = 1; i < this.nbPoints; ++i) {
-            this.draw2DLine(this.points[(i-1)*2+0], this.points[(i-1)*2+1],
-                            this.points[(i-0)*2+0], this.points[(i-0)*2+1],
-                            hueTable[hue]);
+            this.drawMethod([
+                [this.points[(i-1)*2+0], this.points[(i-1)*2+1]],
+                [this.points[(i-0)*2+0], this.points[(i-0)*2+1]],
+                [Math.round(this.width / 2), Math.round(this.height / 2)]],
+                hueTable[hue]);
             hue += hueStep;
         }
-        this.draw2DLine(this.points[(i-1)*2+0], this.points[(i-1)*2+1],
-                        this.points[0], this.points[1], hueTable[hue]);
+        this.drawMethod([
+            [this.points[(i-1)*2+0], this.points[(i-1)*2+1]],
+            [this.points[0], this.points[1]],
+            [Math.round(this.width / 2), Math.round(this.height / 2)]],
+            hueTable[hue]);
+
     }
 
-    plot(x, y, rgb) {
-        this.data[y * this.width + x] =
-            (255    << 24) |  // alpha
-            (rgb[2] << 16) |  // blue
-            (rgb[1] <<  8) |  // green
-            (rgb[0]      );   // red
+    drawFilledPoly(verts, rgb)
+    {
+        // FIXME: write optimized version for triangles?
+        // FIXME: can we tighten those boundaries?
+        var yMin = 0;
+        var yMax = this.height - 1;
+        var xMin = 0;
+        var xMax = this.width - 1;
+
+        var nr = verts.length;
+        var nodeX = new Array(nr + 1);
+        var nodes = 0;
+        var i = 0;
+        var j = 0;
+        var y = 0;
+
+        // Loop through the rows of the image
+        for (y = yMin; y < yMax; ++y) {
+            // Build a list of nodes
+            nodes = 0;
+            j = nr - 1;
+            for (i = 0; i < nr; ++i) {
+                if ((verts[i][1] < y && verts[j][1] >= y) ||
+                    (verts[j][1] < y && verts[i][1] >= y)) {
+                    nodeX[nodes++] = Math.round(verts[i][0] +
+                                                 ((y - verts[i][1]) / (verts[j][1] - verts[i][1])) *
+                                                 (verts[j][0] - verts[i][0]));
+                }
+                j = i;
+            }
+
+            // Sort the nodes
+            nodeX.sort();
+
+            // Fill the pixels between node pairs
+            for (i = 0; i < nodes; i += 2) {
+                if (nodeX[i] >= xMax) break;
+                if (nodeX[i + 1] >  xMin) {
+                    if (nodeX[i    ] < xMin) nodeX[i    ] = xMin;
+                    if (nodeX[i + 1] > xMax) nodeX[i + 1] = xMax;
+                    for (j = nodeX[i]; j < nodeX[i + 1]; j++) {
+                        this.plot(j - xMin, y - yMin, rgb, 255);
+                    }
+                }
+            }
+        }
     }
 
-    draw2DLine(x0, y0, x1, y1, rgb) {
+    drawPoly(verts, rgb) {
+        for (var i = 1; i < verts.length; ++i)
+            this.drawLine(verts[i - 1], verts[i], rgb);
+        this.drawLine(verts[i - 1], verts[0], rgb);
+    }
+
+    drawLine(v0, v1, rgb) {
+        var x0 = v0[0];
+        var y0 = v0[1];
+        var x1 = v1[0];
+        var y1 = v1[1];
         var dx = Math.abs(x1 - x0);
         var dy = Math.abs(y1 - y0);
-        var sx = (x0 < x1) ? 1 : -1;
+        var sx = (x0 < x1) ? 1 : -1;  // FIXME: try "(x0 < x1) * 2 - 1" and see if it's faster
         var sy = (y0 < y1) ? 1 : -1;
         var err = dx - dy;
 
@@ -129,5 +190,13 @@ class Soft3D {
                 y0 += sy;
             }
         }
+    }
+
+    plot(x, y, rgb) {
+        this.data[y * this.width + x] =
+            (255    << 24) |  // alpha
+            (rgb[2] << 16) |  // blue
+            (rgb[1] <<  8) |  // green
+            (rgb[0]      );   // red
     }
 }
